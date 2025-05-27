cfbcd57187d49974aee0e354a149274450b4b059
Boxis3 <ildar.ganiev99@mail.ru>
2025-05-27 19:52:46 +0300

    add new feature

--- GanievIR/Program.cs
+++ GanievIR/Program.cs
@@ -1,4 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
-// Ганиев Ильдар Равилевич, Ам-1-24
-// solve task2
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        // Ганиев Ильдар Равилевич, Ам-1-24
+        // solve task2
+    }
+
+    static public double Sqr(double X)
+    {
+        return X * X;
+    }
+}
